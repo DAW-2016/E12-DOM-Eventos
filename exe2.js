52b1551d12839e4dc713394d3bb5dfc11a1b2bb5
Marcelo Medeiros de Lima <marcelolima381@outlook.com>
2016-05-29 00:36:30 -0300

    Exercício 2 - Final
    Eu não encontrei nenhuma maneira mais decente de sinalizar a restrição
    ao usuário usando somente DOM, mas acho que ficou bom.

--- exe2.js
+++ exe2.js
@@ -1,9 +1,18 @@
-var string = document.body.getElementsByTagName("input")[0].value
-for (var i = 0; i < string.length; i++) {
-    if(((string.charCodeAt(i) >= 65) && (string.charCodeAt(i) <= 90)) || ((string.charCodeAt(i) >= 97) && (string.charCodeAt(i) <= 122))) {
-        console.log("letra");
-    }
-    else{
-        console.log("nao");
-    }
+function checkNameContent() {
+  var string = document.body.getElementsByTagName("input")[0].value;
+  for (var i = 0; i < string.length; i++) {
+      if(((string.charCodeAt(i) >= 65) && (string.charCodeAt(i) <= 90)) || ((string.charCodeAt(i) >= 97) && (string.charCodeAt(i) <= 122))) {
+          console.log("letra");
+          document.getElementsByClassName("form-group")[0].setAttribute("class", "form-group has-success");
+          document.getElementsByClassName("form-group")[1].setAttribute("class", "form-group has-success");
+          document.getElementsByClassName("form-group")[2].setAttribute("class", "form-group has-success");
+      }
+      else{
+          console.log("nao");
+          document.getElementsByClassName("form-group")[0].setAttribute("class", "form-group has-error");
+          document.getElementsByClassName("form-group")[1].setAttribute("class", "form-group has-success");
+          document.getElementsByClassName("form-group")[2].setAttribute("class", "form-group has-success");
+          document.getElementsByTagName("input")[0].value = "SOMENTE LETRAS";
+      }
+  }
 }
